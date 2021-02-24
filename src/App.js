7796419c821f3ff96f0d4551d65065c7c7099ba3
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-02-24 19:32:00 +0530

    All the dependencies and the main root files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Link, Switch,Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import ArtWork from './components/Artwork';
+import CartPage from './components/CartPage';
+import Checkout from './components/Checkout';
+import './components/styles/style.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router >
+      <div className="container">
+      <nav className="navBar">
+      <ul className="navBar__list">
+      <li ><Link className="redirecting__links" to='/'>Home</Link></li>
+      <li><Link className="redirecting__links" to='/LoginPage'>Login</Link></li>
+      <li><Link className="redirecting__links" to='/RegisterPage'>Register</Link></li>
+      <li><Link className="redirecting__links" to='/Artwork'>Art-Work</Link></li>
+      <li><Link className="redirecting__links" to='/CartPage'>Cart</Link></li>
+      <li><Link className="redirecting__links"  to='/Checkout'>Checkout</Link></li>
+      </ul>
+      </nav>
+      <Switch>
+      <Route exact path='/'><HomePage/></Route>
+      <Route exact path='/LoginPage'><LoginPage/></Route>
+      <Route exact path='/RegisterPage'><RegisterPage/></Route>
+      <Route exact path='/Artwork'><ArtWork/></Route>
+      <Route exact path='/CartPage'><CartPage/></Route>
+      <Route exact path='/Checkout'><Checkout/></Route>
+     
+     </Switch>
       </div>
+      </Router>
     );
   }
 }
