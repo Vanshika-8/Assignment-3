1f134925a5dc4bc1b8f98e5821a018a45fc43b67
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-02-25 20:18:48 +0530

    Added two forms login and register which are redirected to homepage after submission.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class LoginPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div></div>
-         );
+    render() {
+        return (
+
+            <div className="maincontainer__form">
+                <form className="login__form">
+                    <h1 className="login__heading">LOGIN Form</h1>
+                    <div className="login__username">
+                        <label>UserName</label>
+                        <input placeholder="Enter Username" type="text" />
+                    </div>
+                    <div className="login__email">
+                        <label>  Email </label>
+                         <input placeholder="Enter email" type="text" />
+                    </div>
+                    <div className="login__password">
+                        <label>  Password </label> 
+                        <input placeholder="Enter password" type="password" />
+                    </div>
+
+                    <Link  to="/"><button className="login__submitbtn">Submit</button></Link>
+                </form>
+            </div>
+
+
+        );
     }
 }
- 
+
 export default LoginPage;
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class RegisterPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
+    render() {
         return (
-            <div></div>
-          );
+            <div className="maincontainer__form">
+                <form className="register__form">
+                    <h1 className="register__heading">Register Here</h1>
+                    <div className="register__username">
+                        <label>Email</label>
+                        <input placeholder="Enter email" type="text" />
+                    </div>
+                    <div className="register__email">
+                        <label>Password</label>
+                        <input placeholder="Enter password" type="password" />
+                    </div>
+                    <div className="register__password">
+                        <label>Confirm-Password</label>
+                        <input placeholder="Confirm-password" type="password" />
+                    </div>
+
+                    <Link to="/"><button className="register__submitbtn">Submit</button></Link>
+                </form>
+            </div>
+
+        );
     }
 }
- 
+
 export default RegisterPage;
