8802182de3dd48e8dd6bff05d7c166c3d9d80518
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-03-18 17:06:14 +0530

    Removed the bugs

--- src/reusableComponents/button/index.js
+++ src/reusableComponents/button/index.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Arrow } from '../Logo';
-import './styles/button-styles.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Arrow } from "../Logo";
+import "./styles/button-styles.css";
+import { Link } from "react-router-dom";
 
-export const Button=(props)=>{
-return(
-    <Link to='/' className="backtohome__btn" >
-   
-    <span className="backtohome__link">  <Arrow className="arrow__logo"/> {props.title}</span>
+export const Button = (props) => {
+  return (
+    <Link to="/" className="backtohome__btn">
+      <span className="backtohome__link">
+        {" "}
+        <Arrow className="arrow__logo" /> {props.title}
+      </span>
     </Link>
-)
-}
+  );
+};
 
-export const AddToCart=(props)=>{
+export const AddToCart = (props) => {
+  const addingItems = (item) => {
+    props.clickHandler(props.item);
+  };
 
-    const addingItems=(item)=>{
-
-props.clickHandler(props.item)
-    }
-
-    return(
-       
-        <div className="backtohome__btn" onClick={addingItems}>
-
-    <div className="addtocart__link"> {props.title}</div>
+  return (
+    <div className="backtohome__btn" onClick={addingItems}>
+      <div className="addtocart__link"> {props.title}</div>
     </div>
-    )
-}
+  );
+};
